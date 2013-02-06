37302655404b0eacee87732ca637ab54b0eed6a3
koyachi <rtk2106@gmail.com>
2013-02-06 23:44:09 +0900

    remove sample code

--- command.go
+++ command.go
@@ -6,35 +6,6 @@ import (
 	"log"
 )
 
-func _searchTest(word string) {
-	var err error
-
-	//records, err := boomkat.Search("Tim Hecker")
-	records, err := boomkat.Search(word)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, record := range records {
-		fmt.Printf("[%s] artist = %s, title = %s, label = %s, genre = %s, url = %s\n",
-			record.Id, record.Artist, record.Title, record.Label, record.Genre, record.Url())
-
-		if record.Id == "598941" {
-			sampleTracks, err := record.SampleTracks()
-			if err != nil {
-				log.Fatal(err)
-			}
-			for i, track := range sampleTracks {
-				if i == 2 {
-					err = track.Download()
-					if err != nil {
-						log.Fatal(err)
-					}
-				}
-			}
-		}
-	}
-}
-
 func search(word string) {
 	tag := fmt.Sprintf("CMD:SEARCH:%s", word)
 	fmt.Printf("%s:START\n", tag)
